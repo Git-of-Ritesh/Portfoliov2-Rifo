0e4e8d5dfef77f9ad1bb643bc61ba3ccf0cc6ed3
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-10-31 19:31:09 +0530

    buid fix footer file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Navbar";
-import Footer from "./Components/footer";
+import Footer from "./Components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
